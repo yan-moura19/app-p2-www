4cb9df4fadfbcef271cff65aa72246276e891848
yan-moura19 <yangabriel3301@gmail.com>
2023-05-29 20:38:15 -0300

    tudo ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,28 +9,68 @@ onMounted(()=>{
   getAtividades()
 })
 const getAtividades = ()=>{
-  axios.get('https://localhost:7037/atividade')
+  
+  axios.get(url.value)
         .then(response => {
           items.value = response.data;
-          console.log(response.data)
+          
         })
         .catch(error => {
           console.error(error);
         });
+}
+const createAtividade = () => {
+      axios.post(url.value, { nome: nome.value})
+        .then(response => {
+          
+          nome.value = '';
+          getAtividades();
+          rota.value = 'home'
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    };
+const putAtividade= async (atividade) => {
+  let idAtividade = atividade.id;
+  let nomeEditar = atividade.nome
+  
+  await axios.put(url.value + "/" + idAtividade, {nome: nomeEditar})
+              .then(()=>{
+                alert("editado");
+                 rota.value = 'home';
+                 getAtividades();
+                 editar.value = {}
+                }).catch('erro ao editar')
+  
+}
+async function finalizar(atividade){
+  let idAtividade = atividade.id;
+  
+  await axios.put(url.value + '/finalizar', {id: idAtividade})
+  .then(()=>
+  {alert("Atividade Finalizada");
+  getAtividades();
+}).catch(()=>{
+  alert("atividade não finalizada, tente novamente")
+})
+  
 
 }
+async function  deleteAtividade(atividade){
+  let idAtividade = atividade.id;
+  await axios.delete(url.value + "/" + idAtividade)
+  .then(()=>getAtividades())
+  .catch(()=>alert('erro ao deletar'))
+}
 
-const url = ref('')
+const url = ref('http://localhost:3000/api/atividade')
 const count = ref(0)
 const rota = ref("home")
 const nome = ref("")
 const editar = ref({})
 const items = ref([
-  {dataCadastro: '20/12/2022', nome: "Atividade 1", status: 'Finalizada', dataFinalizacao: "27/05/2023"},
-  {dataCadastro: '21/12/2022', nome: "Atividade 2", status: 'Em andamento', dataFinalizacao: ""},
-  {dataCadastro: '23/12/2022', nome: "Atividade 3", status: 'Em andamento', dataFinalizacao: ""},
-  {dataCadastro: '24/12/2022', nome: "Atividade 4", status: 'Em andamento', dataFinalizacao: ""},
-  {dataCadastro: '25/12/2022', nome: "Atividade 5", status: 'Em andamento', dataFinalizacao: ""},
+  
   
   ])
 </script>
@@ -57,7 +97,7 @@ const items = ref([
       <br><br>
            <v-main >
             <div v-if="rota === 'editar'">
-              <v-form v-model="formEditar">
+              <v-form >
                   <v-container>
                     <v-row>
                       <v-col
@@ -74,13 +114,13 @@ const items = ref([
                         ></v-text-field>
                       </v-col>   
                     </v-row>
-                    <v-btn type="button" @click="rota = 'home'" block class="mt-2">Submit</v-btn>
+                    <v-btn type="button" @click="putAtividade(editar)" block class="mt-2">Salvar edição</v-btn>
                   </v-container>
                 </v-form>
               </div>
         <div v-if="rota === 'novo'">
 
-          <v-form v-model="valid">
+          <v-form >
               <v-container>
                 <v-row>
                   <v-col
@@ -89,14 +129,14 @@ const items = ref([
                   >
                     <v-text-field
                       v-model="nome"
-                      :rules="nameRules"
+                      
                       :counter="10"
                       label="Nome da atividade"
                       required
                     ></v-text-field>
                   </v-col>   
                 </v-row>
-                <v-btn type="button" @click="rota = 'home'" block class="mt-2">Submit</v-btn>
+                <v-btn type="button" @click="createAtividade" block class="mt-2">Enviar atividade</v-btn>
               </v-container>
             </v-form>
         </div> 
@@ -107,21 +147,22 @@ const items = ref([
                 <v-row class="mt-0  mb-0 pl-4 mr-2 py-2" >
                   <span>{{ item.nome }}</span>
                 <v-spacer></v-spacer>
-                 <v-btn  @click="rota = 'editar';editar = item" type="button" icon="mdi-pencil"  width="40" ></v-btn></v-row>
+                 <v-btn  @click="rota = 'editar';editar = item" type="button" icon="mdi-pencil"  width="40" ></v-btn>
+                 <v-btn  @click="deleteAtividade(item)" type="button" icon="mdi-trash-can-outline"  width="40" ></v-btn></v-row>
                 
               </template>
               <template v-slot:subtitle>
                 Data de cadastro: {{ item.dataCadastro }}
               </template>
               <template v-slot:text>
-                <p v-if="item.dataFinalizacao !== ''">Finalizada dia: {{ item.dataFinalizacao }} <v-icon color="blue">mdi-check-all</v-icon></p>
+                <p v-if="item.atvStatus === 'Finalizada'">Finalizada dia: {{ item.dataFinalizacao }} <v-icon color="blue">mdi-check-all</v-icon></p>
                 <p v-else> Não finalizada <v-icon >mdi-check</v-icon></p>
               </template>
               <v-card-actions  v-if="item.status != 'Finalizada'">
                 
                 <v-btn 
-                v-if="item.status !== 'Finalizada'" 
-                @click="items[index].status = 'Finalizada';items[index].dataFinalizacao = '27/05/2023'" 
+                v-if="item.atvStatus !== 'Finalizada'" 
+                @click="finalizar(item)" 
                 type="button"   
                 class="mt-2">Finalizar
               </v-btn>
